54e25a4efec5a9f899ac4e16d9ff27ee7bfd34b7
1nstagram <sysinvaider@gmail.com>
2025-06-10 03:15:13 -0300

    Update khanvulvMinimal.js

--- khanvulvMinimal.js
+++ khanvulvMinimal.js
@@ -128,7 +128,7 @@ function setupMain(){
                 for (const q of selectorsToCheck) {
                     findAndClickBySelector(q);
                     if (document.querySelector(q+"> div") && document.querySelector(q+"> div").innerText === "Mostrar resumo") {
-                        sendToast("???? Exercício concluído!", 3000);
+                        sendToast("🧟‍♂️ Exercício concluído!", 3000);
                         playAudio("https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/4x5g14gj.wav");
                     }
                 }
